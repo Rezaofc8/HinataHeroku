77a254b3358b8a7f5788a85c26cf72299cffd023
Rezaofc8 <109743116+Rezaofc8@users.noreply.github.com>
2022-08-17 00:41:28 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,77 +1,89 @@
 import fetch from 'node-fetch'
-import { tiktokdl, tiktokdlv2 } from '@bochilteam/scraper'
+import { tiktokdl } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let pp = await conn.profilePictureUrl(who).catch(_ => hoppai.getRandom())
 let name = await conn.getName(who)
 
 if (!args[0]) throw `Use example ${usedPrefix}${command} https://vt.tiktok.com/ZSwWCk5o/`
-
+let buttons = [
+	{ buttonText: { displayText: 'Donasi ≧ω≦' }, buttonId: `${usedPrefix}donasi ${args[0]}` },
+	{ buttonText: { displayText: 'Owner ≥3≤' }, buttonId: `${usedPrefix}owner ${args[0]}` }
+	]
 if (command == 'tiktok') {
-    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
-    const url = video.no_watermark || video.no_watermark_hd || video.with_watermark || video.no_watermark_raw
+    const { author: { nickname }, video, description } = await tiktokdl(args[0])
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
-let caption = `
-🧏 Nickname: ${nickname}
-🔗 Url: ${await shortUrl(url)}
+let caption = `*TIKTOK DOWNLOADER*
+*Nickname:* ${nickname}
+*Description:* ${description}
 
-Description: ${description}`
-    conn.sendButton(m.chat, caption, nickname + '.mp4', await(await fetch(url)).buffer(), [['🎀 Menu', '/menu']], m, { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: {
-            mimetype: 'video/mp4',
-          externalAdReply :{
-    mediaUrl: sig,
-    mediaType: 2,
-    description: wm, 
-    title: '👋 Hai, ' + name + ' ' + ucapan,
-    body: botdate,
-    thumbnail: await(await fetch(pp)).buffer(),
-    sourceUrl: url
-     }}
-  })
+_©${global.wm}_`
+	conn.sendMessage(m.chat, { video: { url: url }, caption: caption , footer: await shortUrl(url), buttons }, { quoted: fgif })
 }
 
 if (command == 'tiktokwm') {
-let linkwm = `https://api.lolhuman.xyz/api/tiktokwm?apikey=${global.lolkey}&url=${args[0]}`
+let linkwm = `https://api.lolhuman.xyz/api/tiktokwm?apikey=9b817532fadff8fc7cb86862&url=${args[0]}`
 if (!linkwm) throw 'Can\'t download video!'
-let caption = `
-Url: ${await shortUrl(args[0])}`
-    conn.sendButton(m.chat, caption, args[0] + '.mp4', await(await fetch(linkwm)).buffer(), [['🎀 Menu', '/menu']], m, { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: {
-            mimetype: 'video/mp4',
-          externalAdReply :{
-    mediaUrl: sig,
-    mediaType: 2,
-    description: wm, 
-    title: '👋 Hai, ' + name + ' ' + ucapan,
-    body: botdate,
-    thumbnail: await(await fetch(pp)).buffer(),
-    sourceUrl: linkwm
-     }}
-  })
+let caption2 = `*TIKTOK WITH WM*
+_©${global.wm}_`
+	// conn.sendMessage(m.chat, { video: { url: linkwm }, caption: caption2 , footer: await shortUrl(`${args[0]}`), buttons }, { quoted: fgif })
+	let buttonMessage= {
+'video': {'url': linkwm},
+'mimetype': "video/mp4",
+'fileLength': fsizedoc,
+'jpegThumbnail': await (await fetch(pp)).buffer(),
+'contextInfo': {
+'externalAdReply': {
+'showAdAttribution': true,
+'mediaUrl': gcwangsaf,
+'mediaType': 2,
+'previewType': 'pdf',
+'title': `👋 Hai, ${name}` ,
+'body': 'Role ',
+'thumbnail': await (await fetch(hwaifu.getRandom())).buffer(),
+'sourceUrl': gcwangsaf}},
+'caption': caption2,
+'footer': await shortUrl(`${args[0]}`),
+'buttons': buttons,
+'headerType': 6}
+    await conn.sendMessage(m.chat, buttonMessage, {quoted: fgif})
+    
 }
 
 if (command == 'tiktoknowm') {
-let link = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${global.lolkey}&url=${args[0]}`)
+let link = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=9b817532fadff8fc7cb86862&url=${args[0]}`)
 let has = await link.json()
 let x = has.result
 if (!x.link) throw 'Can\'t download video!'
-let caption = `
-Title: ${x.title}
-Keyword: ${x.keywords}
-Description: ${x.description}
-Url: ${await shortUrl(x.link)}`
-    conn.sendButton(m.chat, caption, x.title + '.mp4', await(await fetch(x.link)).buffer(), [['🎀 Menu', '/menu']], m, { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: {
-            mimetype: 'video/mp4',
-          externalAdReply :{
-    mediaUrl: sig,
-    mediaType: 2,
-    description: wm, 
-    title: '👋 Hai, ' + name + ' ' + ucapan,
-    body: botdate,
-    thumbnail: await(await fetch(pp)).buffer(),
-    sourceUrl: x.link
-     }}
-  })
+let caption3 = `*TIKTOK NO WM*
+*Title:* ${x.title}
+*Keyword:* ${x.keywords}
+*Description:* ${x.description}
+
+_©${global.wm}_`
+let buttonMessage= {
+'video': {'url': x.link},
+'mimetype': "video/mp4",
+'fileLength': fsizedoc,
+'jpegThumbnail': await (await fetch(pp)).buffer(),
+'contextInfo': {
+'externalAdReply': {
+'showAdAttribution': true,
+'mediaUrl': gcwangsaf,
+'mediaType': 2,
+'previewType': 'pdf',
+'title': `👋 Hai, ${name}` ,
+'body': 'Role ',
+'thumbnail': await (await fetch(hwaifu.getRandom())).buffer(),
+'sourceUrl': gcwangsaf}},
+'caption': caption3,
+'footer': await shortUrl(x.link),
+'buttons': buttons,
+'headerType': 6}
+    await conn.sendMessage(m.chat, buttonMessage, {quoted: fgif})
+	//conn.sendMessage(m.chat, { video: { url: x.link }, caption: caption3 , footer: await shortUrl(x.link), buttons }, { quoted: fgif })
 }
 
 if (command == 'tiktokdl') {
@@ -79,19 +91,11 @@ if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/
     const { author: { nickname }, video, description } = await tiktokdl(args[0])
     const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
-    let caption = `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}\n➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}\nᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}`
-    conn.sendButton(m.chat, caption, nickname + '.mp4', await(await fetch(url)).buffer(), [['🎀 Menu', '/menu']], m, { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: {
-            mimetype: 'video/mp4',
-          externalAdReply :{
-    mediaUrl: sig,
-    mediaType: 2,
-    description: wm, 
-    title: '👋 Hai, ' + name + ' ' + ucapan,
-    body: botdate,
-    thumbnail: await(await fetch(pp)).buffer(),
-    sourceUrl: url
-     }}
-  })
+    conn.sendHydrated(m.chat, `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}`,`➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await(await fetch(url)).buffer(), 
+url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null,null, [
+['ᴅᴏɴᴀꜱɪ', `.donasi`],
+[null,null],
+[null,null]], m)
 }
 
 }
